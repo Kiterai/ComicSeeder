dbfd079e1b798b29d7b58ba257b1cce540fa4245
kiterai <kiterai@foolslab.net>
2023-09-22 07:57:53 +0900

    useKeyboard

---
+++ frontend/src/composables/useKeyboard.ts
@@ -0,0 +1,14 @@
+import { onMounted, onUnmounted } from 'vue';
+
+const useKeyboard = (onkeydown: (e: KeyboardEvent) => void, onkeyup: (e: KeyboardEvent) => void) => {
+  onMounted(() => {
+    window.addEventListener('keydown', onkeydown);
+    window.addEventListener('keyup', onkeyup);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener('keydown', onkeydown);
+    window.removeEventListener('keyup', onkeyup);
+  });
+  return {};
+};
