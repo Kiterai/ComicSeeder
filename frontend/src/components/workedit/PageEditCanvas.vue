f458846aef5aaf168ccb083f2af726c1274b2e25
kiterai <kiterai@foolslab.net>
2023-09-24 07:30:54 +0900

    0枚に出来るバグ修正

--- frontend/src/components/workedit/PageEditCanvas.vue
+++ frontend/src/components/workedit/PageEditCanvas.vue
@@ -164,7 +164,7 @@ async function tryGotoNextPage() {
 async function tryDeleteNowPage() {
   if (pageLoading) return;
   pageLoading = true;
-  if (workPagesStore.pages.length > 0) {
+  if (workPagesStore.pages.length > 1) {
     workPagesStore.pages.splice(workPagesStore.nowPage, 1);
     workPagesStore.nowPage = Math.min(workPagesStore.nowPage, workPagesStore.pages.length - 1);
     await loadNowPage();
