f1d1b01b0f2452049fd35dde9d064b7488c2a1bd
kiterai <kiterai@foolslab.net>
2023-09-23 11:26:47 +0900

    word

--- frontend/src/components/workedit/PageEditTools.vue
+++ frontend/src/components/workedit/PageEditTools.vue
@@ -12,19 +12,56 @@ const onClickPen = () => {
 const onClickEraser = () => {
   drawModeStore.mode = 'eraser';
 };
+const onClickWord = () => {
+  drawModeStore.mode = 'word';
+};
 </script>
 
 <template>
   <div :class="$style.tools">
-    <button :onclick="onClickMove">move</button>
-    <button :onclick="onClickPen">pen</button>
-    <button :onclick="onClickEraser">eraser</button>
+    <button
+      :onclick="onClickMove"
+      :class="$style.tool"
+      :dat-tool-active="drawModeStore.mode == 'move'"
+    >
+      move
+    </button>
+    <button
+      :onclick="onClickPen"
+      :class="$style.tool"
+      :dat-tool-active="drawModeStore.mode == 'pen'"
+    >
+      pen
+    </button>
+    <button
+      :onclick="onClickEraser"
+      :class="$style.tool"
+      :dat-tool-active="drawModeStore.mode == 'eraser'"
+    >
+      eraser
+    </button>
+    <button
+      :onclick="onClickWord"
+      :class="$style.tool"
+      :dat-tool-active="drawModeStore.mode == 'word'"
+    >
+      word
+    </button>
+    <button :class="$style.tool">settings</button>
   </div>
 </template>
 
 <style module>
 .tools {
   position: fixed;
+  left: 0;
   bottom: 0;
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+}
+
+.tool {
+  flex-grow: 1;
 }
 </style>
--- frontend/src/stores/drawMode.ts
+++ frontend/src/stores/drawMode.ts
@@ -1,7 +1,7 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
-export type DrawMode = 'move' | 'pen' | 'eraser';
+export type DrawMode = 'move' | 'pen' | 'eraser' | 'word';
 
 export const useDrawMode = defineStore('drawMode', () => {
   const mode = ref<DrawMode>('pen');
