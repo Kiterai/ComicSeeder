979aff3c7011bb3364768e957c0a778f00807e07
kiterai <kiterai@foolslab.net>
2023-09-24 09:33:05 +0900

    デフォの色変え

--- frontend/src/stores/drawState.ts
+++ frontend/src/stores/drawState.ts
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia';
 export const useDrawState = defineStore('drawState', () => {
   const penWidth = ref(10);
   const penWidthList = ref([5, 10, 20, 40, 50]);
-  const penColor = ref('#888');
+  const penColor = ref('#444');
   const eraserWidth = ref(10);
   const eraserWidthList = ref([5, 10, 20, 40, 50]);
   const nowLayer = ref(0);
