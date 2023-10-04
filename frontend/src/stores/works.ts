2ba7d07a38b124ab35c2b58e42aa826964060fed
kiterai <kiterai@foolslab.net>
2023-10-05 02:32:49 +0900

    作品一覧情報の保存

--- frontend/src/stores/works.ts
+++ frontend/src/stores/works.ts
@@ -10,29 +10,7 @@ type WorkData = {
 };
 
 export const useWorks = defineStore('works', () => {
-  const works = ref<WorkData[]>([
-    {
-      id: '1',
-      title: 'test1',
-      pageNum: 42,
-      createdAt: '-',
-      updatedAt: '-'
-    },
-    {
-      id: '2',
-      title: 'test2',
-      pageNum: 17,
-      createdAt: '-',
-      updatedAt: '-'
-    },
-    {
-      id: '3',
-      title: 'test3',
-      pageNum: 28,
-      createdAt: '-',
-      updatedAt: '-'
-    }
-  ]);
+  const works = ref<WorkData[]>([]);
 
   const generateNewId = () => {
     return works.value.length.toString(); // TODO: uuid
@@ -40,14 +18,49 @@ export const useWorks = defineStore('works', () => {
 
   const addWork = () => {
     const newId = generateNewId();
-    works.value.push({
+    const newWork = {
       id: newId,
       title: 'untitled',
       pageNum: 1,
       createdAt: '-',
       updatedAt: '-'
-    });
+    };
+
+    const req = window.indexedDB.open('ComicSeederDB');
+    req.onupgradeneeded = (e) => {
+      const db = req.result;
+      db.createObjectStore('works', {
+        keyPath: 'id'
+      });
+    };
+    req.onsuccess = (e) => {
+      const db = req.result;
+      const tra = db.transaction('works', 'readwrite');
+      const objStore = tra.objectStore('works');
+      objStore.add(newWork);
+    };
+
+    works.value.push(newWork);
     return newId;
+  };
+
+  const req = window.indexedDB.open('ComicSeederDB');
+  req.onupgradeneeded = (e) => {
+    const db = req.result;
+    db.createObjectStore('works', {
+      keyPath: 'id'
+    });
+  };
+  req.onsuccess = (e) => {
+    const db = req.result;
+    const tra = db.transaction('works', 'readonly');
+    const objStore = tra.objectStore('works');
+    const q = objStore.getAll();
+    q.onsuccess = (e) => {
+      for (const workOnDb of q.result) {
+        works.value.push(workOnDb);
+      }
+    };
   };
 
   return { works, addWork };
