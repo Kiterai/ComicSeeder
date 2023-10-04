44b8d8b909c98036a8099ad1f979b3974bc7e404
kiterai <kiterai@foolslab.net>
2023-10-04 15:14:19 +0900

    消しゴムでかくした

--- frontend/src/stores/drawState.ts
+++ frontend/src/stores/drawState.ts
@@ -5,7 +5,7 @@ export const useDrawState = defineStore('drawState', () => {
   const penWidth = ref(10);
   const penWidthList = ref([5, 10, 20, 40, 50]);
   const penColor = ref('#444');
-  const eraserWidth = ref(10);
+  const eraserWidth = ref(100);
   const eraserWidthList = ref([5, 10, 20, 40, 50]);
   const nowLayer = ref(0);
   return { penWidth, penWidthList, penColor, eraserWidth, eraserWidthList, nowLayer };
