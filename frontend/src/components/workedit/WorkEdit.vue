ee6e25f6d05fdccdd0295a98c7b8108c877aece2
kiterai <kiterai@foolslab.net>
2023-10-12 20:13:30 +0900

    定期セーブ

--- frontend/src/components/workedit/WorkEdit.vue
+++ frontend/src/components/workedit/WorkEdit.vue
@@ -4,15 +4,26 @@ import PageEdit from './PageEdit.vue';
 import WorkSettings from './WorkSettings.vue';
 import PageEditTools from './PageEditTools.vue';
 import { useDrawState } from '@/stores/drawState';
-import { onUnmounted } from 'vue';
+import { onMounted, onUnmounted } from 'vue';
 import { useOpeHistory } from '@/stores/opeHistory';
+import { useWorkPages } from '@/stores/workPages';
 
 const drawState = useDrawState();
 const opeHistory = useOpeHistory();
+const workPage = useWorkPages();
+
+let saveTimer = -1;
+
+onMounted(() => {
+  saveTimer = setInterval(() => {
+    workPage.saveCurrentPage();
+  }, 15000);
+});
 
 onUnmounted(() => {
   drawState.currentPageIndex = 0;
   opeHistory.clearHistory();
+  clearInterval(saveTimer);
 });
 </script>
 
