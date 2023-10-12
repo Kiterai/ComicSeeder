aaad4c42f4f5c1057e8f171b966ce4641dd74b9f
kiterai <kiterai@foolslab.net>
2023-10-12 14:45:53 +0900

    修正

--- frontend/src/components/icons/IconTriangleLeft.vue
+++ frontend/src/components/icons/IconTriangleLeft.vue
@@ -1,5 +1,5 @@
 <template>
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor">
-    <path d="M15 10l-9 5v-10l9 5z"></path>
+    <path d="M14 5v10l-9-5 9-5z"></path>
   </svg>
 </template>
