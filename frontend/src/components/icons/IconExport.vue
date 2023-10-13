10e97ee33eee924b2ef6df6d0e86b51f0d1eed80
kiterai <kiterai@foolslab.net>
2023-10-13 17:02:42 +0900

    アイコン変更

--- frontend/src/components/icons/IconExport.vue
+++ frontend/src/components/icons/IconExport.vue
@@ -1,12 +1,7 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="32"
-    height="32"
-    fill="currentColor"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor">
     <path
-      d="M26 2h-20l-6 6v21c0 0.552 0.448 1 1 1h30c0.552 0 1-0.448 1-1v-21l-6-6zM20 20v6h-8v-6h-6l10-8 10 8h-6zM4.828 6l2-2h18.343l2 2h-22.343z"
+      d="M16 18l8-8h-6v-8h-4v8h-6zM23.273 14.727l-2.242 2.242 8.128 3.031-13.158 4.907-13.158-4.907 8.127-3.031-2.242-2.242-8.727 3.273v8l16 6 16-6v-8z"
     ></path>
   </svg>
 </template>
