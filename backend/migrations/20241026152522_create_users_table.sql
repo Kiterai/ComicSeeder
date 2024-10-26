9f4cc9ba803bc521147d1871ac65f47bf6511007
kiterai <kiterai@foolslab.net>
2024-10-27 00:59:57 +0900

    unique

--- backend/migrations/20241026152522_create_users_table.sql
+++ backend/migrations/20241026152522_create_users_table.sql
@@ -1,8 +1,8 @@
 -- Add migration script here
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    verification_token VARCHAR(255) NOT NULL,
+    verification_token VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
