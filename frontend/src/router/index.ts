f9a96de2f32f1d461e3cb6beefb00c2c7d8e1e94
kiterai <kiterai@foolslab.net>
2024-10-29 01:30:31 +0900

    パスワードリセット画面

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import TopViewVue from '@/views/TopView.vue'
 import WorksViewVue from '@/views/WorksView.vue'
 import WorkEditViewVue from '@/views/WorkEditView.vue'
+import PasswordResetVue from '@/views/PasswordReset.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       name: 'workedit',
       component: WorkEditViewVue,
       props: true
+    },
+    {
+      path: '/password-reset',
+      name: 'password-reset',
+      component: PasswordResetVue
     }
   ]
 })
