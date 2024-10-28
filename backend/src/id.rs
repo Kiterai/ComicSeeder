368176512df2bf4dcad93ca69d89ea5569bca29f
kiterai <kiterai@foolslab.net>
2024-10-28 15:11:40 +0900

    メール送信処理の共通化

--- backend/src/id.rs
+++ backend/src/id.rs
@@ -107,48 +107,21 @@ struct SignupRequest {
 }
 
 async fn send_verification_main(email: &str, token: &str) -> AppResult<()> {
-    let site_name = env::var("SITE_NAME").unwrap_or("ComicSeeder".into());
-    let mailaddr_from = env::var("SMTP_MAILADDR")?;
     let site_url = env::var("FRONT_ORIGIN")?;
-    let smtp_encryption = env::var("SMTP_ENCRYPTION")?;
-    let smtp_host = env::var("SMTP_HOST")?;
-
     let verification_url = format!("{}/verification?{}", site_url, token);
 
-    let email = Message::builder()
-        .header(lettre::message::header::ContentType::TEXT_PLAIN)
-        .from(mailaddr_from.parse()?)
-        .to(Mailbox::new(None, email.parse()?))
-        .subject(format!("{}: ユーザー登録の確認", site_name))
-        .body(format!(
+    send_mail(
+        email,
+        "ユーザー登録の確認",
+        format!(
             "登録を完了するためには10分以内に以下のURLにアクセスしてください。\n\
             {}\n\
             このメールに心当たりがない場合は破棄して頂くようお願いします。",
             verification_url
-        ))?;
-
-    let mut mailer = match smtp_encryption.as_str() {
-        "starttls" => SmtpTransport::starttls_relay(&smtp_host).unwrap(),
-        "tls" => SmtpTransport::relay(&smtp_host).unwrap(),
-        "plain" => SmtpTransport::builder_dangerous(&smtp_host),
-        x => {
-            panic!("invalid SMTP_ENCRYPTION value: {}", x);
-        }
-    };
-
-    if let Ok(smtp_port) = env::var("SMTP_PORT") {
-        mailer = mailer.port(smtp_port.parse()?);
-    }
-    if let (Ok(smtp_user), Ok(smtp_password)) = (env::var("SMTP_USER"), env::var("SMTP_PASSWORD")) {
-        let cred = Credentials::new(smtp_user, smtp_password);
-        mailer = mailer.credentials(cred);
-    }
-
-    let mailer = mailer.build();
-
-    mailer.send(&email)?;
-
-    Ok(())
+        )
+        .as_str(),
+    )
+    .await
 }
 
 #[post("/signup")]
